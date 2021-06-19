6fded355b4dba177f6712e32a83c50af08d8a859
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-19 01:26:31 +0000

    Deploy ChrisXJH/rabbit-and-pig-gomoku to github.com/ChrisXJH/rabbit-and-pig-gomoku.git:gh-pages

---
+++ precache-manifest.77a28a2481cd7d5a341b71d804834b82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d07b61693c39afe731e48ff641ddfc",
+    "url": "/rabbit-and-pig-gomoku/index.html"
+  },
+  {
+    "revision": "470b22c1b9d29600b7ed",
+    "url": "/rabbit-and-pig-gomoku/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c9db40fbb7155d5bf6a5",
+    "url": "/rabbit-and-pig-gomoku/static/js/2.dfed138e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/rabbit-and-pig-gomoku/static/js/2.dfed138e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470b22c1b9d29600b7ed",
+    "url": "/rabbit-and-pig-gomoku/static/js/main.58e78f02.chunk.js"
+  },
+  {
+    "revision": "320646279d82e1aaf48a",
+    "url": "/rabbit-and-pig-gomoku/static/js/runtime-main.484eda97.js"
+  }
+]);
